c8ec79e730f5a8fb4728b4a4a0c3e7691675bc0e
Diana <dianaltecson@gmail.com>
2017-11-23 16:41:57 -0800

    initial letter constructor

--- letters.js
+++ letters.js
@@ -2,7 +2,29 @@
 // or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter.
 // This should contain letter specific logic and data.
 
-// takes in word
-function Letter (word) {
-    this.letters = word.split('');
+var currentWord = require('./word.js');
+
+var letters = currentWord.makeLetters();
+var placeholder = currentWord.makeUnderscores();
+
+var letterData = [];
+
+// iterate through list of letters and create new objects for them -- store in array of objects
+for (var i=0; i < letters.length; i++) {
+    letterData.push(new Letter (letters[i], i));
 }
+
+console.log(JSON.stringify(letterData));
+
+// takes in word
+function Letter (letter, index) {
+    // letter = one of letters of the current word
+    this.letter = letter,
+    // index = array of index where the letter appears in the word
+    this.index = index,
+    // guessed = boolean; if letter has been guessed correctly -- initially set to false
+    this.guess = false
+}
+
+console.log(letterData);
+console.log('hello');
